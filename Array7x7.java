2b93fff28ab24187d323c3f509b5b4106b602ab5
dame910517 <45669889+dame910517@users.noreply.github.com>
2018-12-21 12:25:35 +0100

    Update Array7x7.java

--- Array7x7.java
+++ Array7x7.java
@@ -9,66 +9,69 @@ public class Array7x7 {
 	}
 
 	public Array7x7(Array7x7 array7x7) {
-		for (int i = 0; i < array2.length - 1; i++) {
-			for (int j = 0; j < array2[0].length - 1; j++) {
+		for (int i = 0; i < array2.length; i++) {
+			for (int j = 0; j < array2[0].length; j++) {
 				int ny = array7x7.getElement(i, j);
 				array2[i][j] = ny;
 			}
 		}
 	}
 
-	public Array7x7(int[][] array) { // RÄTT?
-		for (int i = 0; i < array2.length - 1; i++) {
-			for (int j = 0; j < array2[0].length - 1; j++) {
-				array2[i][j]++; // RÄTT?
+	public Array7x7(int[][] array) { 
+		for (int i = 0; i < array2.length; i++) {
+			for (int j = 0; j < array2[0].length; j++) {
+				array2[i][j]++;
 			}
 		}
 	}
 
 	public void setElement(int row, int col, int value) {
-		if (row >= 0 && row < array2.length - 1 && col >= 0 && col < array2.length - 1) {
+		if (row >= 0 && row < array2.length && col >= 0 && col < array2.length) {
 			array2[row][col] = value;
 		}
 	}
 
-	public int getElement(int row, int col) { // VARFÖR FEL?
-		if (row >= 0 && row < array2.length - 1 && col >= 0 && col < array2.length - 1) {
+	public int getElement(int row, int col) { 
+		if (row >= 0 && row < array2.length && col >= 0 && col < array2.length) {
 			return array2[row][col];
 		}
 	}
 
-//	Skapa metoder som kan skriva och läsa rader och kolumner:
+//	Skapa metoder som kan skriva och lÃ¤sa rader och kolumner:
 
-	public void setRow(int row, Array7 theRow) { // måste använda metod i klassen array7
-//		if (row >= 0 && row < array2.length - 1 && theRow >= 0 && theRow < array2.length) {
-//			array2[row] = theRow.getElement(row); //nu är det rätt med typer
-//			array2[row] = array7.getElement(theRow); //detta känns mer logiskt
+	public void setRow(int row, Array7 theRow) { 
 		for (int i = 0; i < array2.length; i++) {
-			array2[i] = array7.getElement(row);
+			array2[row][i] = theRow.getElement(i);
 		}
 	}
 
-//	for (int i = 0; i < array.length-1; i++) {
-//		int ny = array7.getElement(i);
-//		array[i] = ny;
-//	}
-
 	public Array7 getRow(int row) {
-
+		Array7 ny = new Array7();
+		for (int i = 0; i < array2[row].length; i++) {
+			ny.setElement(i, array2[row][i]);
+		}
+		return ny;
 	}
 
 	public void setCol(int col, Array7 theCol) {
-
+		for(int i = 0; i< array2.length; i++) {
+			array2[i][col]=theCol.getElement(i);
+		}
 	}
 
 	public Array7 getCol(int col) {
+		Array7 ny = new Array7();
+		for (int i = 0; i < array2[col].length; i++) {
+			ny.setElement(i, array2[i][col]);
+		}
+		return ny;
 	}
 
-//	Skapa metoder som kan skriva och läsa samliga element:
+//	Skapa metoder som kan skriva och lÃ¤sa samliga element:
 
-	public void setArray(Array7x7 array7x7) { //SAMMA SOM KONSTRUKTORN?
-		for (int i = 0; i < array2.length - 1; i++) {
-			for (int j = 0; j < array2[0].length - 1; j++) {
+	public void setArray(Array7x7 array7x7) { 
+		for (int i = 0; i < array2.length ; i++) {
+			for (int j = 0; j < array2[0].length ; j++) {
 				int ny = array7x7.getElement(i, j);
 				array2[i][j] = ny;
 			}
@@ -81,8 +84,8 @@ public class Array7x7 {
 
 	public Array7x7 getArray() { // ny instans av int[] ska returneras
 		Array7x7 ny = new Array7x7();
-		for (int i = 0; i < array2.length - 1; i++) {
-			for (int j = 0; j < array2[0].length - 1; j++) {
+		for (int i = 0; i < array2.length ; i++) {
+			for (int j = 0; j < array2[0].length ; j++) {
 				array2[i][j] = ny.getElement(i, j);
 			}
 		}
@@ -91,8 +94,8 @@ public class Array7x7 {
 
 	public int[][] toIntArray() { // ny instans av int[] ska returneras
 		int[][] ny = new int[7][7];
-		for (int i = 0; i < array2.length - 1; i++) {
-			for (int j = 0; j < array2[0].length - 1; j++) {
+		for (int i = 0; i < array2.length ; i++) {
+			for (int j = 0; j < array2[0].length ; j++) {
 				array2[i][j] = ny[i][j];
 			}
 		}
