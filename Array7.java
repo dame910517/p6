78cfba12184c905d9a5bbafe6d28a3e92d1bc834
dame910517 <45669889+dame910517@users.noreply.github.com>
2018-12-21 11:53:38 +0100

    Update Array7.java

--- Array7.java
+++ Array7.java
@@ -1,41 +1,41 @@
 package p6;
 
-public class Array7 { //körde array.length-1 överallt pga värdet sätts t int[7]
+public class Array7 { 
 	private int[] array;
 
 	public Array7() { // default
 		array = new int[7];
 	}
 
-	public Array7(Array7 array7) {//obejt innehåller array med flera värden
+	public Array7(Array7 array7) {
 
-		for (int i = 0; i < array.length-1; i++) {
+		for (int i = 0; i < array.length; i++) {
 			int ny = array7.getElement(i);
 			array[i] = ny;
 		}
 	}
 	
 
-	public Array7(int[] array) { //  RÄTT?
-		for (int i = 0; i < array.length-1; i++) {
-			array[i]++; // lägge ri värden i array, RÄTT?
+	public Array7(int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			array[i]++; 
 		}
 	}
 
 	public void setElement(int pos, int value) {
-		if (pos < array.length - 1 && pos > 0) {
+		if (pos < array.length && pos > 0) {
 			array[pos] = value; 
 		}
 	}
 
 	public int getElement(int pos) { // VARFÖR FEL? vill hitta positionen eller värdet?
-		while (pos < array.length - 1 && pos >= 0) {
+		while (pos < array.length && pos >= 0) {
 			return array[pos];
 		}
 	}
 
 	public void setArray(Array7 array7) { //SKA VARA SAMMA SOM KONSTURKTORN? N
-		for (int i = 0; i < array.length-1; i++) {
+		for (int i = 0; i < array.length; i++) {
 			int ny = array7.getElement(i);
 			array[i] = ny;
 		}
@@ -48,7 +48,7 @@ public class Array7 { //körde array.length-1 överallt pga värdet sätts t int
 
 	public Array7 getArray() { // ny instans av Array7 ska returneras
 		Array7 ny = new Array7();
-		for (int i = 0; i < array.length-1; i++) {
+		for (int i = 0; i < array.length; i++) {
 			array[i]  = ny.getElement(i);
 		}
 		return ny;
@@ -57,7 +57,7 @@ public class Array7 { //körde array.length-1 överallt pga värdet sätts t int
 
 	public int[] toIntArray() { // ny instans av int[] ska returneras
 		int[] array2 = new int[7];
-		for(int i = 0; i < array.length-1; i++) {
+		for(int i = 0; i < array.length; i++) {
 			array2[i] = array[i];
 		}
 		return array2;
