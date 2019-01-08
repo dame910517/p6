2ee9dd2b5dea3ac4a2127348e970ffbc5e684ff8
settlers99 <45967339+settlers99@users.noreply.github.com>
2019-01-08 10:16:15 +0100

    Add files via upload

--- Controller2.java
+++ Controller2.java
@@ -20,22 +20,38 @@ public class Controller2
 	public Controller2()
 	{
 	
-	ui = new Test2UI();
 	
 	array = new Array7x7();
 	rightColumn = new Array7();
 	leftColumn = new Array7();
 	
-	for(int i = 0; i >= 7; i++)
+	for(int i = 0; i < 7; i++)
 	{
 		rightColumn.setElement(i, rng.nextInt(8));
 		leftColumn.setElement(i, rng.nextInt(8));
+		
+		for(int j = 0; j < 0; j++)
+		{
+			array.setElement(i, j, rng.nextInt(8));
+		}
 	}
 	
-	ui = new Test2UI();
 	
-	ui.setRight(rightColumn);
-	ui.setLeft(leftColumn);
+	ui = new Test2UI(this.leftColumn, this.rightColumn, this.array);
+	
+	
+	}
+	
+	public Controller2(Array7 mLeftColumn, Array7 mRightColumn, Array7x7 mArray)
+	{
+	
+	
+	array = mArray;
+	rightColumn = mRightColumn;
+	leftColumn = mLeftColumn;
+	
+	ui = new Test2UI(this.leftColumn, this.rightColumn, this.array);
+	
 	
 	}
 	
@@ -56,7 +72,41 @@ public class Controller2
 	
 	public static void main(String [] args)
 	{
-		Controller2 controller = new Controller2();
+
+		Random rng = new Random();
+		
+	    Array7 testArray1 = new Array7();
+	    Array7 testArray2 = new Array7();
+	    Array7 testArray4 = new Array7();
+	    
+	    for(int i = 0; i < 7; i++)
+		{
+			testArray1.setElement(i, rng.nextInt(9));
+			
+		}
+	    
+	    for(int i = 0; i < 7; i++)
+		{
+			testArray2.setElement(i, rng.nextInt(9));
+			
+		}
+	    int[] s = {4, 6, 6, 6, 7, 8, 6};
+	    testArray4.setArray(s);
+	    
+	    int[][] test = {{7, 6, 6, 6, 7, 8, 9}, 
+	    		{2, 6, 6, 6, 7, 8, 9}, 
+	    		{7, 51, 6, 6, 7, 8, 9},
+	    		{1, 6, 2, 6, 7, 8, 9},
+	    		{6, 1, 6, 6, 7, 8, 9},
+	    		{3, 6, 4, 6, 7, 8, 16},
+	    		{9, 6, 6, 6, 21, 8, 12}};
+	    
+	    Array7x7 testArray3 = new Array7x7(test);
+		
+	   // Controller2 controller = new Controller2();
+		Controller2 controller = new Controller2(testArray4, testArray2, testArray3);
+		
+		
 		
 		JFrame frame1 = new JFrame( "SSPViewer" );
 		frame1.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
