e57f540cbb74433c0022f22f9e7dfa47e763a4e2
dame910517 <45669889+dame910517@users.noreply.github.com>
2019-01-11 11:23:18 +0100

    Update Display.java

--- Display.java
+++ Display.java
@@ -15,12 +15,13 @@ import javax.swing.*;
 public class Display extends JPanel {
 	private Timer timer;
 
+
 	private ImageIcon imgSetText = new ImageIcon("Images/settext2.jpg");
 	private ImageIcon imgChooseImage = new ImageIcon("Images/chooseimage.jpg");
 
-	private JPanel pnlColorPanel = new JPanel( new BorderLayout() );
-	private JPanel pnlArrows = new JPanel( new GridLayout(3, 3) );
-	private JPanel pnlSouth = new JPanel( new GridLayout(1, 3) );
+	private JPanel pnlColorPanel = new JPanel(new BorderLayout());
+	private JPanel pnlArrows = new JPanel(new GridLayout(3, 3));
+	private JPanel pnlSouth = new JPanel(new GridLayout(1, 3));
 
 	// Knappar för att styra bokstävernas flöde i fönstret.
 	private JButton btnUpLeft = new JButton("↖");
@@ -33,46 +34,48 @@ public class Display extends JPanel {
 	private JButton btnDown = new JButton("↓");
 	private JButton btnDownRight = new JButton("↘");
 
+
 	private JFileChooser fileChooser = new JFileChooser();
 
 	// Knappar för att sätta texten
-	private JButton btnSetText = new JButton( imgSetText );
-	private JButton btnBigRight = new JButton( imgChooseImage );
+	private JButton btnSetText = new JButton("Skriv in en text"); //JButton(imgSetText);
+	private JButton btnBigRight = new JButton("Välj en bild"); //(imgChooseImage);
 
 	private MainController mc;
-	private ColorDisplay cd = new ColorDisplay(7,10,Color.WHITE,Color.BLACK,1,10);
+	private ColorDisplay cd = new ColorDisplay(7, 10, Color.WHITE, Color.BLACK, 1, 10);
 
-	public Display( MainController mc ) {
-		this.mc = mc; 
-		mc.setDisplaySize( 7, 10 );
+	public Display(MainController mc) {
+		this.mc = mc;
+		mc.setDisplaySize(7, 10);
 		mc.fillArrayRandom();
 		mc.finalizeDisplayArray();
 
-		setPreferredSize( new Dimension ( 755, 630 ) );
-		setLayout( new BorderLayout() );
+		setPreferredSize(new Dimension(755, 630));
+		setLayout(new BorderLayout());
 
-		// pnlArrows innehåller pilarna för att användaren ska kunna styra i vilken riktning
+		// pnlArrows innehåller pilarna för att användaren ska kunna styra i vilken
+		// riktning
 		// texten rör sig över fönstret
-		pnlArrows.add( btnUpLeft );
-		pnlArrows.add( btnUp );
-		pnlArrows.add( btnUpRight );
-		pnlArrows.add( btnLeft );
-		pnlArrows.add( btnCenter );
-		pnlArrows.add( btnRight );
-		pnlArrows.add( btnDownLeft );
-		pnlArrows.add( btnDown );
-		pnlArrows.add( btnDownRight );
+		pnlArrows.add(btnUpLeft);
+		pnlArrows.add(btnUp);
+		pnlArrows.add(btnUpRight);
+		pnlArrows.add(btnLeft);
+		pnlArrows.add(btnCenter);
+		pnlArrows.add(btnRight);
+		pnlArrows.add(btnDownLeft);
+		pnlArrows.add(btnDown);
+		pnlArrows.add(btnDownRight);
 
-		pnlSouth.add( btnSetText );
-		pnlSouth.add( pnlArrows );
-		pnlSouth.add( btnBigRight );
+		pnlSouth.add(btnSetText);
+		pnlSouth.add(pnlArrows);
+		pnlSouth.add(btnBigRight);
 
-		pnlColorPanel.add( cd );
+		pnlColorPanel.add(cd);
 
-		add( pnlColorPanel, BorderLayout.NORTH );
-		add( pnlSouth, BorderLayout.SOUTH );
+		add(pnlColorPanel, BorderLayout.NORTH);
+		add(pnlSouth, BorderLayout.SOUTH);
 
-		AL listener = new AL();
+		ScrollerListener listener = new ScrollerListener();
 
 		btnUpLeft.addActionListener(listener);
 		btnUp.addActionListener(listener);
@@ -84,106 +87,135 @@ public class Display extends JPanel {
 		btnDown.addActionListener(listener);
 		btnDownRight.addActionListener(listener);
 		btnSetText.addActionListener(listener);
-		
+
 		OpenDirectoryListener odListener = new OpenDirectoryListener();
 		btnBigRight.addActionListener(odListener);
-		
 
 	}
-	
+
 	public void addArray() {
 		Array7x7 array[][] = mc.getDisplayArray();
-		for( int row = 0 ; row < array.length ; row++ ) {
-			for( int col = 0 ; col < array[row].length ; col++ ) {
-				cd.setDisplay( array[row][col].toIntArray(), row, col );
+		for (int row = 0; row < array.length; row++) {
+			for (int col = 0; col < array[row].length; col++) {
+				cd.setDisplay(array[row][col].toIntArray(), row, col);
 			}
 		}
 		cd.updateDisplay();
 	}
-	
+
 	public void pauseText() {
-	}
-	
-	private class Scroller extends TimerTask {
-		public Scroller( JButton btn ) {
-			
-		}
-		public void actionPerformed( ActionEvent e ) {
-			
-		}
-		@Override
-		public void run() {
-			
-		}
-		// konstruktor 
-		// ta emot jbutton-input
-		// scroll listener
-		// stoplistener
 		
 	}
+
+	//rullar ej
+	private class ScrollerListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {//
+
+			ScrollerTask task = new ScrollerTask((JButton)e.getSource());
+			timer = new Timer();
+			timer.schedule(task,  100, 100);
+//			timer.scheduleExecutionTime();
+		}
+	}
+
+	private class ScrollerTask extends TimerTask {
+			private JButton input;
+
+			public ScrollerTask(JButton input) {
+				this.input = input;
+			}
+
+			@Override
+			public void run() {
+			if (input.equals(btnUpLeft)) {
+				mc.shiftTextLeft();
+				mc.shiftTextUp();
+
+				//sätter ny display, behövs pga att shifttextleft går tomma luckor där texten tidigare varit?
+				mc.finalizeDisplayArray();
+				addArray();
+			} else if (input.equals(btnUp)) {
+				mc.shiftTextUp();
+				
+				mc.finalizeDisplayArray();
+				addArray();
+
+			} else if (input.equals(btnUpRight)) {
+				mc.shiftTextUp();
+				mc.shiftTextRight();
+				
+				mc.finalizeDisplayArray();
+				addArray();
+
+			} else if (input.equals(btnLeft)) {
+				mc.shiftTextLeft();
+				
+				mc.finalizeDisplayArray();
+				addArray();
+
+			} else if (input.equals(btnCenter)) {
+				pauseText(); //nödvändig? kan ej bara ha timer.cancel? 
+				mc.finalizeDisplayArray();
+				addArray();
+				timer.cancel();
+			} else if (input.equals(btnRight)) {
+				mc.shiftTextRight();
+				
+				mc.finalizeDisplayArray();
+				addArray();
+
+			} else if (input.equals(btnDownLeft)) {
+				mc.shiftTextDown();
+				mc.shiftTextLeft();
+				
+				mc.finalizeDisplayArray();
+				addArray();
+
+			} else if (input.equals(btnDown)) {
+				mc.shiftTextDown();
+				
+				mc.finalizeDisplayArray();
+				addArray();
+
+			} else if (input.equals(btnDownRight)) {
+				mc.shiftTextDown();
+				mc.shiftTextRight();
+				
+				mc.finalizeDisplayArray();
+				addArray();
+
+			} else if (input.equals(btnSetText)) {
+				String input = JOptionPane.showInputDialog(null, "Write the text you wish to display");
+				mc.setText(input);
+				mc.finalizeDisplayArray();
+				addArray();
+				timer.cancel();
+			}
+			mc.getDisplayArray();
+		}
+	}
+
 	
+
 	private class OpenDirectoryListener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
-			int ret = fileChooser.showOpenDialog( null );
-			if( e.getSource() == btnBigRight ) {
-				if( ret == JFileChooser.APPROVE_OPTION ) {
-					Picture picture = new Picture( fileChooser.getSelectedFile() );	
+			int ret = fileChooser.showOpenDialog(null);
+			if (e.getSource() == btnBigRight) {
+				if (ret == JFileChooser.APPROVE_OPTION) {
+					Picture picture = new Picture(fileChooser.getSelectedFile());
 				}
 			}
 		}
 	}
-
-	private class AL implements ActionListener { 
-		public void actionPerformed(ActionEvent e) {
-			if( e.getSource() == btnUpLeft ) {
-				mc.shiftTextLeft();
-				
-			} else if( e.getSource() == btnUp ) {
-				mc.shiftTextUp();
-				
-			} else if( e.getSource() == btnUpRight ) {
-				mc.shiftTextUp();
-				mc.shiftTextRight();
-				
-			} else if( e.getSource() == btnLeft ) {
-				mc.shiftTextLeft();
-				
-			} else if( e.getSource() == btnCenter ) {
-				pauseText();
-				
-			} else if( e.getSource() == btnRight ) {
-				mc.shiftTextRight();
-
-			} else if( e.getSource() == btnDownLeft ) {
-				mc.shiftTextDown();
-				mc.shiftTextLeft();
-
-			} else if( e.getSource() == btnDown ) {
-				mc.shiftTextDown();
-
-			} else if( e.getSource() == btnDownRight ) {
-				mc.shiftTextDown();
-				mc.shiftTextRight();
-
-			} else if( e.getSource() == btnSetText ) {
-				String input = JOptionPane.showInputDialog(null, "Write the text you wish to display");
-				mc.setText( input );
-				mc.finalizeDisplayArray();
-				addArray();
-//				mc.fillArrayRandom();
-			}
-		}
-	}
-
 	public static void main(String[] args) {
 		MainController mc = new MainController();
-		Display display = new Display( mc );
-		JFrame frame1 = new JFrame( "Display" );
-		frame1.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-		frame1.add( display );
+		Display display = new Display(mc);
+		JFrame frame1 = new JFrame("Display");
+		frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame1.add(display);
+		display.addArray();
 		frame1.setLocationRelativeTo(null);
 		frame1.pack();
-		frame1.setVisible( true );
+		frame1.setVisible(true);
 	}
 }
-
